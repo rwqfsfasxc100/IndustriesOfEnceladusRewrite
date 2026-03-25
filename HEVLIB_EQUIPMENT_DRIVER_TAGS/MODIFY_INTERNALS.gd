7efebe38062c9ee084399045a56bf2590a8d42b6
rwqfsfasxc100 <123430291+rwqfsfasxc100@users.noreply.github.com>
2026-03-25 14:49:28 -0700

    switched harvester class drones to using MODIFY_INTERNALS for drone storage

--- HEVLIB_EQUIPMENT_DRIVER_TAGS/MODIFY_INTERNALS.gd
+++ HEVLIB_EQUIPMENT_DRIVER_TAGS/MODIFY_INTERNALS.gd
@@ -27,6 +27,14 @@ const MODIFY_INTERNALS = [
 		"storage_nanodrones":5000
 	},
 	{
+		"system":"SYSTEM_DND_HARVTUG",
+		"storage_nanodrones":2500
+	},
+	{
+		"system":"SYSTEM_DND_HARVHAUL",
+		"storage_nanodrones":2500
+	},
+	{
 		"system":"SYSTEM_CARGO_MOD_2K",
 		"storage_flat":2000,
 		"mass":1600,
