5a6875a760038849882f8b911443f7193ba9aca9
__hev <123430291+rwqfsfasxc100@users.noreply.github.com>
2026-04-20 21:12:42 -0700

    Update NAMER.gd

--- HEVLIB_EQUIPMENT_DRIVER_TAGS/NAMER.gd
+++ HEVLIB_EQUIPMENT_DRIVER_TAGS/NAMER.gd
@@ -4,6 +4,8 @@ const CREW = [
 	["usa","Pauline","Vo",0],
 	["usa","Brayden","Nguyen",1],
 	["usa","David","Lister",1],
+	["usa","Arnold","Rimmer",1],
+	["usa","Kristine","Kochanski",0],
 	["verbatim","Ryland","Grace",1]
 ]
 
