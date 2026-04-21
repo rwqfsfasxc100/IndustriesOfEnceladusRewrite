36bcadc482cf950b63b5fb2dcde71f891b7952f0
__hev <123430291+rwqfsfasxc100@users.noreply.github.com>
2026-04-20 20:10:48 -0700

    added NameDriver instance

---
+++ HEVLIB_EQUIPMENT_DRIVER_TAGS/NAMER.gd
@@ -0,0 +1,13 @@
+extends Node
+
+const CREW = [
+	["usa","Pauline","Vo",0],
+	["usa","Brayden","Nguyen",1],
+	["usa","David","Lister",1],
+	["verbatim","Ryland","Grace",1]
+]
+
+const SHIPS = [
+	["en","Red","Dwarf"],
+	["verbatim","Hail","Mary"]
+]
