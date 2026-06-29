7e469c239e895ec22244d66d1a41a6147e46380b
__hev <123430291+rwqfsfasxc100@users.noreply.github.com>
2026-06-29 09:21:28 -0700

    Removed old code for the standard excavator on the KX37-RAM

--- ships/modules/Excavator.gd
+++ ships/modules/Excavator.gd
@@ -2,7 +2,7 @@ extends "res://ships/Front Bumper.gd"
 
 export (bool) var removeThis = false
 
-func _ready():
+func _enter_tree():
 	#print("Firing Excavator code")
 	if removeThis:
 		Tool.remove(self)
