35c2b26a02a5aa9e49cbad579fed2f8f7e640d27
__hev <123430291+rwqfsfasxc100@users.noreply.github.com>
2026-07-09 01:36:30 -0700

    IoER is now a modlet

---
+++ HEVLIB_EQUIPMENT_DRIVER_TAGS/LOAD_RESOURCES.gd
@@ -0,0 +1,14 @@
+extends Node
+
+const LOAD_RESOURCES = {
+	"ships/modules/MineralProcessingUnit.patch.gd":{"load_type":"script"},
+	"hud/trtl/ShipParams.tscn":{"load_type":"resource"},
+	"ships/ship-ctrl.gd":{"load_type":"script"},
+	"/hud/AutopilotOverlay.gd":{"load_type":"script"},
+	"sfx/torch-PMS.tscn":{"load_type":"scene"},
+	"sfx/torch-ZUBRIN.tscn":{"load_type":"scene","original_path":"res://sfx/torch-ZUBRIN.tscn"},
+	"weapons/railgun_tor.tscn":{"load_type":"resource","original_path":"res://weapons/railgun_tor.tscn"},
+	"enceladus/Dealer.tscn":{"load_type":"scene"},
+	"enceladus/Upgrades.gd":{"load_type":"script"},
+	"comms/conversation/subtrees/DIALOG_PIRATE_SUPPORT.tscn":{"load_type":"scene","onready":true}
+}
